66fa202e8263c40ecca50552d078c3a95ca9bfcf
“Shirley_Xiong” <sx822@ic.ac.uk>
2023-06-17 20:49:23 +0100

    update_score_but_reset_fail

--- web-app/game.js
+++ web-app/game.js
@@ -16,7 +16,7 @@ function Game() {
    * @function
    * @param {string} action The action to take.
    */
-  game.chooseAction = function(action) {
+game.chooseAction = function(action) {
     if (game.currentPlayer === game.player1) {
       game.action1 = action;
       game.currentPlayer = game.player2;
@@ -31,12 +31,27 @@ function Game() {
     }
   };
 
+  game.restartGame = function() {
+
+  // reset health and defenseMode
+  game.player1.health = 100;
+  game.player2.health = 100;
+  game.player1.defenseMode = false;
+  game.player2.defenseMode = false;
+
+
+  // update the health bars and scores in HTML
+  document.getElementById('player1Health').style.width = game.player1.health + "%";
+  document.getElementById('player2Health').style.width = game.player2.health + "%";
+  document.getElementById('score').innerText = `Score: Player 1 - ${game.player1Score}, Player 2 - ${game.player2Score}`;
+};
+
   /**
    * Resolve a round of combat after both players have chosen an action.
    * @memberof Game
    * @function
    */
-  game.resolveRound = function() {
+game.resolveRound = function() {
     if (game.action1 === 'defend') {
       game.player1.toggleDefenseMode();
     }
@@ -62,46 +77,19 @@ function Game() {
     console.log("Player 2: " + game.player2.health + " health remaining.");
 
    // Check if a player's health has reached zero and increment score
-    if (game.player1.health <= 0) {
-      game.player2Score += 1;
-      console.log("Player 2 wins the round!");
-      game.restartGame();
-      }
-    if (game.player2.health <= 0) {
-      game.player1Score += 1;
-      console.log("Player 1 wins the round!");
-      game.restartGame();
-      }
-    //update the scoreboard in HTML
-    document.getElementById('score').innerText = `Score: Player 1 - ${game.player1Score}, Player 2 - ${game.player2Score}`;};
+   if (game.player1.health <= 0) {
+    game.player2Score += 1;
+    console.log("Player 2 wins the round!");
+    game.restartGame(); // ensure this is only called when health <= 0
+  }
+  if (game.player2.health <= 0) {
+    game.player1Score += 1;
+    console.log("Player 1 wins the round!");
+    game.restartGame(); // ensure this is only called when health <= 0
+  }
+};
 
 
-  game.restartGame = function() {
-    // Increase the score of the winning player
-    if (game.player1.health <= 0) {
-      game.player2Score += 1;
-    } else {
-      game.player1Score += 1;
-     }
-   // Log the scores
-  console.log(`Score: Player 1 - ${game.player1Score}, Player 2 - ${game.player2Score}`);
-
-    // Reset game state
-      game.player1 = CharacterObj.Character("Player 1");
-      game.player2 = CharacterObj.Character("Player 2");
-      game.currentPlayer = game.player1;
-      game.action1 = null;
-      game.action2 = null;
-      game.player1.defenseMode = false;
-      game.player2.defenseMode = false;
-      game.player1Score = 0;
-      game.player2Score = 0;
-
-    // Update the UI
-      document.getElementById('player1Health').style.width = "100%";
-      document.getElementById('player2Health').style.width = "100%";
-      document.getElementById('player').innerText = "Player 1's turn";
-      document.getElementById('score').innerText = `Score: Player 1 - ${game.player1Score}, Player 2 - ${game.player2Score}`;};
   return game;
 }
 
